7643d4cc3e3cf28d17205b7324dbef7deb0f5194
dzjshfu <1531959391@qq.com>
2019-02-18 21:40:23 +0800

    add demo2.java

---
+++ min2phase-dev-min/demo2.java
@@ -0,0 +1,11 @@
+import cs.min2phase.Search;
+import java.io.*;
+
+public class demo2 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String scrambledCube = in.readLine();
+        String result = new Search().solution(scrambledCube, 21, 100000000, 0, 0);
+        System.out.println(result);
+    }
+}
