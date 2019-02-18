a6ce8751a94628e0f305bb2a81e5378c0f3cb053
dzjshfu <1531959391@qq.com>
2019-02-18 21:49:31 +0800

    change a parameter

--- min2phase-dev-min/demo2.java
+++ min2phase-dev-min/demo2.java
@@ -5,7 +5,7 @@ public class demo2 {
     public static void main(String[] args) throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String scrambledCube = in.readLine();
-        String result = new Search().solution(scrambledCube, 21, 100000000, 0, 0);
+        String result = new Search().solution(scrambledCube, 25, 100000000, 0, 0);
         System.out.println(result);
     }
 }
