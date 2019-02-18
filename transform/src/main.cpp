d9e0b17ddfe177673e8633290334cd23f2a72d1c
lostcandyx <515586472@qq.com>
2019-02-19 01:43:58 +0800

    Test version for uart on raspberry 3B+

--- transform/src/main.cpp
+++ transform/src/main.cpp
@@ -4,8 +4,43 @@
 //  Date:2019.2.18
 //  Entry to the whole program
 
+//
 #include <transform.h>
 #include <Robot.h>
+//Used for UART on raspberry
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
+int main(int argc, char const *argv[])
+{
+    int fd;
+    unsigned char message;
+    Transform *tessst = new Transform();
+    test->mainControl();
+
+    if (wiringPiSetup() < 0)
+        return 1;
+
+    if ((fd = serialOpen((char *)"/dev/ttyAMA0", 9600)) < 0)
+        return 1;
+
+    printf("serial test start ...\n");
+    std::cout << "minCost: " << test->getMinCost() << std::endl;
+    test->printCharResult();
+
+    message = test->getMessage();
+    serialPrintf(fd, (char *)message);
+
+    //while(1)
+    //{
+    //	serialPutchar(fd,serialGetchar(fd));
+    //	printf("can recive the message..\n");
+    //}
+    serialClose(fd);
+    return 0;
+}
+
+/*
 int main(int argc, char const *argv[])
 {
     // Transform* trf = new Transform();
@@ -36,3 +71,4 @@ int main(int argc, char const *argv[])
     return 0;
     
 }
+*/
